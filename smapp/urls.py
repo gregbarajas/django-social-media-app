729e0d05b3e18e68e6508497024261d15dcb4aa2
gregbarajas <gregbarajas96@gmail.com>
2023-06-27 09:35:31 -0700

    Added new Posts user model in model.py
    Added settings.html to 'templates/smapp'.

--- smapp/urls.py
+++ smapp/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('signup', views.signup, name='signup'),
     path('signin', views.signin, name='signin'),
+    path('settings', views.settings, name='settings'),
+    path('logout', views.logout, name='logout'),
     ]
--- smapp/views.py
+++ smapp/views.py
@@ -1,13 +1,32 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User, auth
-from django.contrib import messages
-from .models import Profile
+from django.contrib import messages, auth
+from .models import Profile, Post
 
 # Create your views here.
-
+@login_required(login_url='signin')
 def index(request):
-    return render(request, 'smapp/index.html')
+    user_object = User.objects.get(username=request.user.username)
+    user_profile = Profile.objects.get(user=user_object)
+    return render(request, 'smapp/index.html', {'user_profile': user_profile})
+
+@login_required(login_url='signin')
+def upload(request):
+
+    if request.method == 'POST':
+        user = request.user.username
+        image = request.FILES.get('image_upload')
+        caption = request.POST['caption']
+
+        new_post = Post.objects.create(user=user, image=image, caption=caption)
+        new_post.save()
+
+        return redirect('/')
+    else:
+        return redirect('/')
+
 
 def signup(request):
     if request.method == 'POST':
@@ -44,6 +63,7 @@ def signup(request):
         return render(request, 'smapp/signup.html')
 
 def signin(request):
+
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
@@ -58,4 +78,37 @@ def signin(request):
             return redirect('signin')
 
     else:
-        return render(request, 'signin.html')
+        return render(request, 'smapp/signin.html')
+
+@login_required(login_url='signin')
+def logout(request):
+    auth.logout(request)
+    return redirect('signin')
+
+@login_required(login_url='signin')
+def settings(request):
+    user_profile = Profile.objects.get(user=request.user)
+
+    if request.method == 'POST':
+
+        if request.FILES.get('image') == None:
+            image = user_profile.profileimg
+            bio = request.POST['bio']
+            location = request.POST['location']
+
+            user_profile.profileimg = image
+            user_profile.bio = bio
+            user_profile.location = location
+            user_profile.save()
+        if request.FILES.get('image') != None:
+            image = request.FILES.get('image')
+            bio = request.POST['bio']
+            location = request.POST['location']
+
+            user_profile.profileimg = image
+            user_profile.bio = bio
+            user_profile.location = location
+            user_profile.save()
+
+        return redirect('settings')
+    return render(request, 'smapp/settings.html', {'user_profile': user_profile})
